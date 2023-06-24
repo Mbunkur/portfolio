7f07803c25a1abd7a4fb4d8b67b0a8cec2a6b831
Mbunkur <alainmbunkur@gmail.com>
2023-06-24 06:40:11 +0200

    update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,108 +1,48 @@
-import React from 'react'
-import './portfolio.css'
-import IMG1 from '../../assets/img1.png'
-import IMG2 from '../../assets/img2.png'
-import IMG3 from '../../assets/img3.png'
-import IMG4 from '../../assets/img4.png'
-import IMG5 from '../../assets/img5.png'
-import IMG6 from '../../assets/img6.png'
-import IMG7 from '../../assets/img7.png'
-import IMG8 from '../../assets/img8.png'
-import IMG9 from '../../assets/img9.png'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './portfolio.css';
 
 const Portfolio = () => {
-  const data = [
-    {
-      image : IMG1,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 1,
-      title:'Project 1'
-    },
-    {
-      image : IMG2,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 2,
-      title:'Project 2'
-    },
-    {
-      image : IMG3,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 3,
-      title:'Project 3'
-    },
-    {
-      image : IMG4,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 4,
-      title:'Project 4'
-    },
-    {
-      image : IMG5,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 5,
-      title:'Project 5'
-    },
-      {
-      image : IMG6,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 6,
-      title:'Project 6'
-    },
-    {
-      image : IMG7,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 7,
-      title:'Project 7'
-    },
-    {
-      image : IMG8,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 8,
-      title:'Project 8'
-    },
-    {
-      image : IMG9,
-      github: 'https://github.com',
-      demo: 'https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design',
-      id: 9,
-      title:'Project 9'
-    },
-    
-  ]
-       
-  return (
-    <section id="portfolio">
-      <h5>My recent work</h5>
-      <h2>Portfolio</h2>
+  const [data, setData] = useState([]);
 
-      <div className="container portfolio__container">
-        {
-          data.map(({id, title, image, github, demo}) =>{
-            return (
-              <article key={id} className="portfolio__item">
-                <div className="portfolio__item-image">
-                  <img src={image} alt={title} />
-                </div>
-                <h3> {title} </h3>
-                <div className="portfolio__item-cta">
-                  <a href={github} className="btn" target="_blank" rel="noreferrer"> Github </a>
-                  <a href={demo} className="btn btn-primary" target="blank" rel="noreferrer">Live Demo</a>
-                </div>
-              </article>
-            )
-          })
-        }
+  useEffect(() => {
+    axios
+      .get('https://portfolio-backend-end.cyclic.app/api/portfolio')
+      .then(response => {
+        const formattedData = response.data.data.map(item => ({
+          id: item._id,
+          image: item.image,
+          title: item.title,
+          github: item.github,
+          demo: item.demo
+        }));
+        setData(formattedData);
+      })
+      .catch(error => {
+        console.error('Error fetching portfolio data:', error);
+      });
+  }, []);
+
+  return (
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio_container'>
+      {data.map(({ id, image, title, github, demo }) => (
+          <article key={id} className='portfolio__item'>
+            <div className="portfolio__item-img">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className='btn' target='_blank' rel='noopener noreferrer'>Github</a>
+              <a href={demo} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Live Demo</a>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
-import './testimonial.css'
-import AV1 from '../../assets/avat1.jpg'
-import AV2 from '../../assets/avat2.jpg'
-import AV3 from '../../assets/avat3.jpg'
-import AV4 from '../../assets/avat4.jpg'
-import AV5 from '../../assets/avat5.jpg'
-
-// import Swiper core and required modules
-import {Pagination, Navigation, Scrollbar, A11y} from 'swiper';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import './testimonials.css'
+import { Pagination} from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -17,66 +11,62 @@ import 'swiper/css/pagination';
 
 
 
-const Testimonial = () => {
-  const data = [
-    {
-      avatar: AV1,
-      name: "Client 1",
-      review:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.',
-            
-    },
-    {
-      avatar: AV2,
-      name: "Client 2",
-      review:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-           
-    },
-    {
-      avatar: AV3,
-      name: "Client 3",
-      review: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      
-    },
-    {
-      avatar: AV4,
-      name: "Client 4",
-      review: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      
-    },
-    {
-      avatar: AV5,
-      name: "Client 5",
-      review:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-           
-    },
-    
-  ]
+const Testimonials = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(()=> {
+    axios
+      .get("https://portfolio-backend-end.cyclic.app/api/testimonial")
+      .then(res =>{
+        const formattedData = res.data.data.map(item => ({
+          id: item._id,
+          avatar: item.avatar,
+          name: item.name,
+          review: item.review,
+        }));
+        console.log(`Here am I: ${formattedData}`);
+        setData(formattedData);
+      } )
+      .catch(err => console.error(err));
+  }, [])
+
+
   return (
-    <section id='testimonial'>
+    <section id='testimonials'>
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-
-      <Swiper className="container testimonial__container"
-      modules={[Pagination, Navigation, Scrollbar, A11y]}
+      <Swiper className="container testimonials__container"
+      
+      modules={[Pagination]}
       spaceBetween={40}
       slidesPerView={1}
-      pagination={{ clickable: true }}>
-        {
-          data.map(({avatar, name, review}, index) => {
-            return(
-              <SwiperSlide  key ={index} className='testimonial'>
-              <div className="client__avatar">
-    Swiper       <img src={avatar} alt='clients'/>      
-              </div>
-              <h5 className='client_name'>{name}</h5>
-                <small className={review}>{review}</small>
-            </SwiperSlide>
-            )
-          })
-        }
+      pagination={{ clickable: true }}
+
+      
+      >
+     
+{
+
+
+
+  data.map(({id, avatar, name, review}) =>(
+    <SwiperSlide key={id} className='testimonial'>
+    <div className='client__avatar'>
+      <img src={avatar} alt={name} />
+    </div>
+    <h5>{name}</h5>
+      <small className='client__review'>
+     {review}
+      </small>
+    </SwiperSlide>
+  ))
+}
+        
       </Swiper>
+      
     </section>
   )
 }
 
-export default Testimonial
+export default Testimonials
+
